94281338a200f108fc9dfa296138fac3376645ea
Lynn <code@krogseng.com>
2016-12-29 15:37:32 -0800

    completed first three tests - check for object creation, Article.toHtml, and existance of allArticles array.

--- starter-code/test/test.js
+++ starter-code/test/test.js
@@ -1,16 +1,31 @@
+var testArticle = new Article({
+  title: 'Transmitting Open-source Arrays',
+  category: 'firewall',
+  author: 'Dr. Tressie Kuphal',
+  authorUrl: 'http://http://corrine.net',
+  publishedOn: '2014-01-22',
+  body: '## The RSS sensor is down, synthesize the open-source sensor so we can synthesize the RSS sensor!\n\ntransmitting the driver won\'t do anything, we need to navigate the optical EXE driver! Rerum sunt quia.\nQui illo saepe inventore facilis eius doloribus.\nEt qui omnis.\nPraesentium et animi eos labore dicta et qui aut facere.\nSunt maiores voluptatibus provident dolorem sed nihil velit.\n \rIpsum rerum et et sit earum aut omnis sint unde.\nInventore ullam molestiae et reiciendis.\n'
+});
+
 QUnit.module('Article Constructor', function() {
   QUnit.test('Article should create a new Arcticle', function(assert) {
     //TODO: write a test for the Article constructor
+
+
+    assert.equal(testArticle.category, 'firewall');
+    assert.equal(testArticle.publishedOn, '2014-01-22');
   });
 
   QUnit.test('Article instance should have a method toHtml', function(assert) {
     //TODO: verify that an instance of article has a toHtml method
+    assert.equal(typeof(testArticle.toHtml),'function');
   });
 });
 
 QUnit.module('Article Class', function() {
   QUnit.test('Article.allArticles should be an array', function(assert) {
     //TODO: write a test for the all articles property
+    assert.equal((Array.isArray(Article.allArticles)), true);
   });
   QUnit.test('Article.loadAll should set an array of Article instances', function(assert) {
     //TODO: write a test for article loadAll
