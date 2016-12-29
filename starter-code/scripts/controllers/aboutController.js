82cad15177e0fadf76a3e058956037b8a11742ee
Aaron Krause <aaroncrows@Aarons-MacBook-Pro.local>
2016-12-29 13:08:43 -0800

    update for testing

--- starter-code/scripts/controllers/aboutController.js
+++
@@ -1,9 +0,0 @@
-(function(module) {
-  var aboutController = {};
-
-  aboutController.reveal = function() {
-    /* TODO: Reveal only the about section! */
-  };
-
-  module.aboutController = aboutController;
-})(window);
--- starter-code/scripts/controllers/articleController.js
+++
@@ -1,9 +0,0 @@
-(function(module) {
-  var articleController = {};
-
-  articleController.reveal = function() {
-    /* TODO: Reveal only the articles section! */
-  };
-
-  module.articleController = articleController;
-})(window);
