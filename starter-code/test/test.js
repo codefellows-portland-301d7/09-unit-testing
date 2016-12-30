6f01935cedb95ec59f827bf968c3032752536edd
Clarefollett <chlaw101@gmail.com>
2016-12-29 16:55:48 -0800

    finished writing the tests

--- starter-code/test/test.js
+++ starter-code/test/test.js
@@ -9,7 +9,7 @@ var testArticle = new Article({
 
 QUnit.module('Article Constructor', function() {
   QUnit.test('Article should create a new Arcticle', function(assert) {
-    //TODO: write a test for the Article constructor
+    //TODO: DONE// write a test for the Article constructor
 
 
     assert.equal(testArticle.category, 'firewall');
@@ -17,23 +17,43 @@ QUnit.module('Article Constructor', function() {
   });
 
   QUnit.test('Article instance should have a method toHtml', function(assert) {
-    //TODO: verify that an instance of article has a toHtml method
+    //TODO: DONE// verify that an instance of article has a toHtml method
     assert.equal(typeof(testArticle.toHtml),'function');
   });
 });
 
 QUnit.module('Article Class', function() {
+  var t1Test =
+    [{
+      title: 'test 1',
+      author: 'test 2'
+    },
+      {
+        title: 'test 2',
+        author: 'test 2'
+      },
+      {
+        title: 'test 3',
+        author: 'test 3'
+      }
+    ];
+
   QUnit.test('Article.allArticles should be an array', function(assert) {
-    //TODO: write a test for the all articles property
+    //TODO: DONE// write a test for the all articles property
     assert.equal((Array.isArray(Article.allArticles)), true);
   });
   QUnit.test('Article.loadAll should set an array of Article instances', function(assert) {
-    //TODO: write a test for article loadAll
+    //TODO: DONE// write a test for article loadAll
     //remember to reset anything that changed about your global
     //environment at the end of the test!
+    Article.loadAll(t1Test);
+    assert.equal(Article.allArticles.length, 3);
+
   });
   QUnit.test('Article.allAuthors should get a unique name of authors', function(assert) {
-    //TODO: write a test for Article.allAuthors
+    //TODO: DONE// write a test for Article.allAuthors
+    Article.loadAll(t1Test);
+    assert.equal(Article.allAuthors().length, 2);
   });
   //STRETCH: write a test for Article.numWordsAll
   //STRETCH: write a test for Article.fetchAll
